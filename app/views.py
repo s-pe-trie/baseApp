6b9af060bd870f769f5885b097bd14b90a84f2f4
tajay59 <tajay59@gmail.com>
2023-02-15 13:53:59 -0500

    Update

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
  """
-from app import app 
+from app import app, Config
 from flask import render_template, request, redirect, url_for, flash,  session,  send_from_directory,  abort 
 from werkzeug.utils import secure_filename 
 from werkzeug.security import check_password_hash
